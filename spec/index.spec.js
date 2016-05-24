50ee97db9a245d9bfb1f7cba9632a86d433d7d89
Benji Marinacci <bcmarinacci@icloud.com>
2016-05-24 02:29:59 -0700

    Update tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -7,12 +7,14 @@ const listFilepaths = require('../');
 describe('listFilepaths', function () {
   it('should return an array containing the paths of all files in a directory and its subdirectories', function (done) {
     const expectedPaths = [
-      resolve('spec/fixtures/utils/higher-order/for-each/for-each.js'),
-      resolve('spec/fixtures/utils/higher-order/map/map.js'),
-      resolve('spec/fixtures/utils/print/print.js')
+      resolve('spec/fixtures/ships/millennium-falcon/millennium-falcon.js'),
+      resolve('spec/fixtures/ships/millennium-falcon/pilots/chewbacca.js'),
+      resolve('spec/fixtures/ships/millennium-falcon/pilots/han-solo.js'),
+      resolve('spec/fixtures/ships/slave-i/pilots/jango-fett.js'),
+      resolve('spec/fixtures/ships/slave-i/slave-i.js')
     ];
 
-    listFilepaths('spec/fixtures/utils')
+    listFilepaths('spec/fixtures/ships')
       .then(filepaths => {
         expect(expectedPaths.length).toEqual(filepaths.length);
         expect(expectedPaths).toEqual(filepaths);
