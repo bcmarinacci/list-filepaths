6a5f1ad45f5b8d0ca56150d6f0ac71f4862eee7d
Benji Marinacci <bcmarinacci@icloud.com>
2016-05-24 15:04:16 -0700

    Add tests for filter option

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -23,6 +23,47 @@ describe('listFilepaths', function () {
       .catch(done.fail);
   });
 
+  it('should optionally filter the list of filepaths using a regular expression', function (done) {
+    const expectedPaths = [
+      resolve('spec/fixtures/ships/slave-i/pilots/boba-fett.js'),
+      resolve('spec/fixtures/ships/slave-i/slave-i.js')
+    ];
+
+    const options = {
+      filter: /slave-i/
+    };
+
+    listFilepaths('spec/fixtures/ships', options)
+      .then(filepaths => {
+        expect(expectedPaths.length).toEqual(filepaths.length);
+        expect(expectedPaths).toEqual(filepaths);
+        done();
+      })
+      .catch(done.fail);
+  });
+
+  it('should optionally filter the list of filepaths using a function', function (done) {
+    const expectedPaths = [
+      resolve('spec/fixtures/ships/millennium-falcon/pilots/chewbacca.js'),
+      resolve('spec/fixtures/ships/millennium-falcon/pilots/han-solo.js'),
+      resolve('spec/fixtures/ships/slave-i/pilots/boba-fett.js'),
+    ];
+
+    const options = {
+      filter(filepath) {
+        return /pilots/.test(filepath);
+      }
+    };
+
+    listFilepaths('spec/fixtures/ships', options)
+      .then(filepaths => {
+        expect(expectedPaths.length).toEqual(filepaths.length);
+        expect(expectedPaths).toEqual(filepaths);
+        done();
+      })
+      .catch(done.fail);
+  });
+
   it('should throw an error for an invalid path', function (done) {
     listFilepaths('invalid/filepath')
       .then(done.fail)
