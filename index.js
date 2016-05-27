7054706fa1c24bebe9d2a0451d4737a0e9e55d73
Benji Marinacci <bcmarinacci@icloud.com>
2016-05-27 02:14:56 -0700

    Refactor to use generators for async control flow

--- index.js
+++ index.js
@@ -2,51 +2,42 @@
 
 const { readdir, stat } = require('fs');
 const { join, resolve } = require('path');
+const co = require('co');
 const flattendeep = require('lodash.flattendeep');
 const pify = require('pify');
 const filterPaths = require('./lib/filter-paths');
 const shouldReject = require('./lib/should-reject');
 
-const createPathTree = function (dirPath, options, currentDepth = 0) {
-  return pify(stat)(dirPath)
-    .then(stats => {
-      if (stats.isFile()) {
-        // Wrap the path in an array in case a filepath is passed in as the initial value
-        return [dirPath];
-      }
-
-      if (currentDepth > options.depth) {
-        return null;
-      }
-
-      return pify(readdir)(dirPath)
-        .then(dirChildren => {
-          const promiseMap = dirChildren.map(dirChild => {
-            const childPath = join(dirPath, dirChild);
-            if (shouldReject(childPath, options.reject)) {
-              return null;
-            }
-
-            return createPathTree(childPath, options, currentDepth + 1);
-          });
-
-          return Promise.all(promiseMap);
-        });
-    });
-};
-
-module.exports = function (inputPath, options = {}) {
+const createPathTree = module.exports = co.wrap(function* (dirPath, options = {}, currentDepth = 0) {
   const targetPath = options.relative
-    ? inputPath
-    : resolve(inputPath);
+    ? dirPath
+    : resolve(dirPath);
 
-  return createPathTree(targetPath, options)
-  .then(pathArr => {
-    const filteredPaths = filterPaths(flattendeep(pathArr), options.filter);
-    if (!filteredPaths.length) {
+  const stats = yield pify(stat)(targetPath);
+  if (stats.isFile()) {
+    // Wrap the path in an array in case a filepath is passed in as the initial value
+    return [targetPath];
+  }
+
+  if (currentDepth > options.depth) {
+    return null;
+  }
+
+  const dirContents = yield pify(readdir)(targetPath);
+  const promisePathTree = dirContents.map(el => {
+    const elPath = join(targetPath, el);
+    if (shouldReject(elPath, options.reject)) {
       return null;
     }
 
-    return filteredPaths;
+    return createPathTree(elPath, options, currentDepth + 1);
   });
-};
+
+  const pathTree = yield Promise.all(promisePathTree);
+  const pathList = filterPaths(flattendeep(pathTree), options.filter);
+  if (!pathList.length) {
+    return null;
+  }
+
+  return pathList;
+});
